368a966050d20f973df92a2b884614d5b7ac95d2
Irshad-creates <ansariirshad78601@gmail.com>
2026-04-26 01:56:34 +0530

    feat: add song upload functionality with multer and ImageKit integration

--- Day-25/Backend/src/app.js
+++ Day-25/Backend/src/app.js
@@ -15,6 +15,8 @@ app.use(cors({
  * Routes
  */
 const authRoutes = require("./routes/auth.routes")
+const songRoutes = require("./routes/song.routes")
 app.use("/api/auth", authRoutes)
+app.use("/api/song",songRoutes)
 
 module.exports = app
---
+++ Day-25/Backend/src/controllers/song.controller.js
@@ -0,0 +1,38 @@
+const songModel = require("../models/song.model")
+const storageService = require("../services/storage.services")
+const id3 = require("node-id3")
+
+async function uploadSong(req, res){
+    const songBuffer = req.file.buffer
+    const {mood} = req.body
+
+    const tags = id3.read(songBuffer)
+    
+    const songFile = await storageService.uploadFile({
+        buffer:songBuffer,
+        filename:tags.title,
+        folder:"/cohort2/Moodify/songs"
+    })
+
+    const posterFile = await storageService.uploadFile({
+        buffer: tags.image.imageBuffer,
+        filename:tags.title +".jpeg",
+        folder:"/cohort2/Moodify/posters"
+    })
+
+    const song = await songModel.create({
+        title : tags.title,
+        url: songFile.url,
+        posterUrl : posterFile.url,
+        mood
+    })
+
+    res.status(200).json({
+        message:"song created succesfully",
+        song
+    })
+}
+
+module.exports = {
+    uploadSong
+};
