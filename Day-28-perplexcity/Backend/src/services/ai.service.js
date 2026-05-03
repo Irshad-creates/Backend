0249b37e46a2b8035a7042b44512e05a39a6635a
Irshad-creates <ansariirshad78601@gmail.com>
2026-05-04 00:43:07 +0530

    feat: implement interactive AI chat service with Google Generative AI

--- Day-28-perplexcity/Backend/src/services/ai.service.js
+++ Day-28-perplexcity/Backend/src/services/ai.service.js
@@ -1,11 +1,58 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage } from "langchain"
+import readline from "readline/promises"
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env")
+});
+
+
+const rl = readline.createInterface({
+  input :process.stdin,
+  output :process.stdout
+})
+
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY
 });
 
-export async function testAi (){
-  model.invoke("how many times has CSK beaten MI in last 5 years")
-  .then((response)=>{console.log(response.text)})
+// export async function testAi (){
+//   model.invoke("what is your name?")
+//   .then((response)=>{console.log(response.text)})
+// }
+
+const message = []
+const AI_NAME = "PerplexCity AI";
+
+const colors = {
+  reset: "\x1b[0m",
+  cyan: "\x1b[36m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m"
+};
+
+while (true){
+  const userInput = await rl.question(
+    `${colors.cyan}You:${colors.reset} `
+  )
+
+  message.push(new HumanMessage(userInput))
+
+  const response = await model.invoke(message)
+
+  message.push(response)
+
+  console.log(
+    `${colors.green}${AI_NAME}:${colors.reset} ${colors.yellow}${response.text}${colors.reset}`
+  )
 }
+
+rl.close()
