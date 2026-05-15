d900e8ebc0a3738d75860e872ade97aebdc612ab
Irshad-creates <ansariirshad78601@gmail.com>
2026-05-16 03:35:30 +0530

    feat: enhance Dashboard layout with new chat navigation and styling improvements

--- Day-28-perplexcity/Frontend/src/features/chat/Pages/Dashboard.jsx
+++ Day-28-perplexcity/Frontend/src/features/chat/Pages/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
     // chat.initializeSocketConnection()
     chat.handleGetChats()
   }, [])
-
+  
   const handleSubmitMessage = (event) => {
     event.preventDefault()
 
@@ -35,10 +35,10 @@ const Dashboard = () => {
   return (
     <main className='min-h-screen w-full bg-black  text-white '>
       <section className='mx-auto  flex h-[calc(100vh-1.5rem)] w-full gap-4 rounded-3xl border    md:gap-6  border-none'>
-        <aside className='relative hidden h-full w-60 shrink-0  border-r  p-4 md:flex md:flex-col'>
+        <aside className='relative hidden h-full w-60 shrink-0  border-r border-zinc-700 p-4 md:flex md:flex-col'>
           
 
-          <nav className='space-y-1 mb-5'>
+          <nav className='space-y-1 mb-4'>
 
             <NavLink to='/' className={({ isActive }) => `w-full flex items-center gap-3 pl-2 py-2 rounded-lg transition-all duration-200
               ${
@@ -68,28 +68,69 @@ const Dashboard = () => {
             <span className='text-sm font-medium'>Chats</span>
             </NavLink>
 
-          </nav>  
+            <NavLink to='/Email' className={({ isActive }) => `w-full flex items-center gap-3 pl-2 py-2 rounded-lg transition-all duration-200
+              ${
+                isActive
+                  ? "bg-zinc-100 text-black dark:bg-zinc-800 dark:text-white"
+                  : "bg-black text-zinc-500 hover:bg-zinc-900 hover:text-zinc-200"
+              }`
+            }
+          >
+            <svg  role='img' className='h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z"></path>
+            </svg>
+            <span className='text-sm font-medium'>Email</span>
+            </NavLink>
 
-          <div className='space-y-2 max-h-100 pr-2 overflow-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-[#000000] '>
-            {Object.values(chats).map((chat,index) => (
-              <button
-                onClick={()=>{openChat(chat.id)}}
-                key={index}
-                type='button'
-                className='w-full cursor-pointer rounded-xl border border-white/20 bg-transparent px-3 py-2 text-left text-base font-medium text-white/90 transition hover:border-white hover:text-white'
-              >
-                {chat.title}
+          </nav>
+
+          <NavLink to="/chat" className={({ isActive }) => `w-full flex items-center gap-3 pl-2 py-2 mb-4 rounded-lg transition-all duration-200
+              ${
+                isActive
+                  ? "bg-zinc-100 text-black dark:bg-zinc-800 dark:text-white"
+                  : "bg-black text-zinc-500 hover:bg-zinc-900 hover:text-zinc-200"
+              }`
+            }
+          >
+          
+          <span className='text-sm font-medium'> +  New chat</span>
+          </NavLink> 
+          
+          <div className="">
+            <h1 className='text-zinc-500 font-bold text-sm mb-3'>RECENT</h1>
+            <div className='space-y-2 max-h-50 pr-2 overflow-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-[#000000] '>
+              {Object.values(chats).map((chat,index) => (
+                <button
+                  onClick={() => { openChat(chat.id) }}
+                  key={index}
+                  type='button'
+                  className='w-full flex items-center cursor-pointer rounded-xl hover:bg-zinc-900 px-2 py-1 text-left text-sm whitespace-nowrap overflow-hidden text-ellipsis font-medium text-white/90 transition hover:border-white hover:text-white'
+                >
+                  {chat.title}
               </button>
-            ))}
+              ))}
+            </div>
           </div>
 
-          <div className='absolute bottom-1  h-12 w-50  bg-zinc-600 ' >
-              <h1>username</h1>
+
+          <div className='absolute bottom-1  h-12 w-50 rounded-lg bg:bg-zinc-800 hover:bg-zinc-900 flex items-center justify-between px-2 ' >
+              <div className='h-2 flex items-center gap-2'>
+                <svg  className='h-8 p-1 border rounded-2xl cursor-pointer bg-zinc-900 text-zinc-400' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H18C18 18.6863 15.3137 16 12 16C8.68629 16 6 18.6863 6 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11Z"></path></svg>
+                <span className='font-medium text-sm'>Username</span>
+              </div>
+
+              <div  className='h-2 flex items-center text-zinc-600'>
+                <svg className='h-6 p-1 cursor-pointer hover:text-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M20 17H22V19H2V17H4V10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10V17ZM18 17V10C18 6.68629 15.3137 4 12 4C8.68629 4 6 6.68629 6 10V17H18ZM9 21H15V23H9V21Z"></path></svg>
+              
+                <svg className='h-6 p-1 cursor-pointer hover:text-red-400' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 22C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V6H18V4H6V20H18V18H20V21C20 21.5523 19.5523 22 19 22H5ZM18 16V13H11V11H18V8L23 12L18 16Z"></path></svg>
+              </div>
           </div>
 
         </aside>
 
-        <section className='relative max-w-3/5 mx-auto flex h-full min-w-0 flex-1 flex-col gap-4'>
+        <section className='relative  w-full mx-auto overflow-hidden   h-screen min-w-0 flex flex-1 flex-col items-center gap-4 '>
+
+          
 
           <div className='messages flex-1 space-y-3 overflow-y-auto pr-1 pb-30'>
             {chats[ currentChatId ]?.messages.map((message) => (
